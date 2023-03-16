cf64590ff496691267c243deb531dae8d0b98fe3
ParkerHitch <parker.hitchcock@gmail.com>
2023-03-16 11:55:45 -0500

    Oops

--- src/players/agents/OSLA_V1.cpp
+++ src/players/agents/OSLA_V1.cpp
@@ -4,31 +4,23 @@
 #include "../Player.h"
 #include "../../utils/Math.h"
 
-float RLBasic_V1::evaluate(GameState& gs, GameAction& ga){
+float OSLA_V1::evaluate(GameState& gs, GameAction& ga){
     GameState* test = new GameState(gs);
     test->applyAction(ga);
+    PlayerState& ps = test->playerStates[id];
+
     float score = 0;
-    for(int i=0; i<4; i++) {
-        PlayerState &ps = test->playerStates[(id+i)%4]; // weights[0] is weight for current player
-        score += weights[i][0] * (float) ps.discounts[0];
-        score += weights[i][1] * (float) ps.discounts[1];
-        score += weights[i][2] * (float) ps.discounts[2];
-        score += weights[i][3] * (float) ps.discounts[3];
-        score += weights[i][4] * (float) ps.discounts[4];
-        score += weights[i][5] * (float) ps.balance0;
-        score += weights[i][6] * (float) ps.balance1;
-        score += weights[i][7] * (float) ps.balance2;
-        score += weights[i][8] * (float) ps.balance3;
-        score += weights[i][9] * (float) ps.balance4;
-        score += weights[i][10] * (float) ps.balanceY;
-        score += weights[i][11] * (float) ps.pts;
-        int nReserved = (ps.reservedCards[0]==nullptr) + (ps.reservedCards[1]==nullptr) + (ps.reservedCards[2]==nullptr);
-        int reservedPts = (ps.reservedCards[0]==nullptr?0:ps.reservedCards[0]->points) +
-                          (ps.reservedCards[1]==nullptr?0:ps.reservedCards[1]->points) +
-                          (ps.reservedCards[2]==nullptr?0:ps.reservedCards[2]->points);
-        score += weights[i][12] * (float) nReserved;
-        score += weights[i][13] * (float) reservedPts;
-    }
+    score += (float)ps.discounts[0];
+    score += (float)ps.discounts[1];
+    score += (float)ps.discounts[2];
+    score += (float)ps.discounts[3];
+    score += (float)ps.discounts[4];
+    score += 0.2f * (float)ps.balance0;
+    score += 0.2f * (float)ps.balance1;
+    score += 0.2f * (float)ps.balance2;
+    score += 0.2f * (float)ps.balance3;
+    score += 0.2f * (float)ps.balance4;
+    score += 0.4f * (float)ps.balanceY;
     delete test;
     return score;
 }
